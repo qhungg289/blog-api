6165b261bd61ecf0299f4df0306e8eba03fef376
Hung Duong Quang <qhungg289@protonmail.com>
2022-01-30 23:05:43 +0700

    Setup basic json API

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const passport = require("passport");
+
+const app = express();
+
+require("./config/db");
+require("./config/auth");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res, next) => {
+	res.json({ msg: "Hello World" });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
