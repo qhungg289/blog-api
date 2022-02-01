e2801f42f279369e6a06eeac8239e01e14425176
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-01 22:53:59 +0700

    Add detail comments for app.js and auth.js

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const morgan = require("morgan");
 const cors = require("cors");
 const passport = require("passport");
 
+// Initialize express
 const app = express();
 
+// Require database and authentication config
 require("./config/db");
 require("./config/auth");
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
+// Apply all necessary middlewares
+app.use(cors()); // Enable Cross Origin Resource Sharing
+app.use(morgan("dev")); // Developement logging
+app.use(express.json()); // Accept incomming json data
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
+app.use(passport.initialize()); // Initialize passport js
 
 app.get("/", (req, res, next) => {
 	res.json({ msg: "Hello World" });
 });
 
+// Get the port number and listen to incoming request
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server started on port: ${port}`));
--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,28 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const AdminModel = require("../models/AdminModel");
 const bcrypt = require("bcryptjs");
 
+// -------------------------
+// Set up local strategy
+// -------------------------
 passport.use(
+	// Create an instance of LocalStrategy class, with session disabled
 	new LocalStrategy({ session: false }, async (username, password, done) => {
 		try {
+			// Find one user from the username
 			const user = await AdminModel.findOne({ username: username });
 
+			// If there is no user, return with no error and no user (false)
 			if (!user) {
 				return done(null, false);
 			}
 
+			// Compare the password with the one in the database,
+			// if not match return with no error and no user (false)
 			if (!(await bcrypt.compare(password, user.password))) {
 				return done(null, false);
 			}
 
+			// Return the user with no error
 			return done(null, user);
 		} catch (error) {
 			return done(error);
@@ -26,6 +35,11 @@ passport.use(
 	})
 );
 
+// -------------------------
+// Set up JWT strategy
+// -------------------------
+
+// Options for the strategy
 const jwtOptions = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 	secretOrKey: process.env.TOKEN_SECRET,
@@ -33,8 +47,10 @@ const jwtOptions = {
 };
 
 passport.use(
+	// Create an instace of JwtStrategy
 	new JwtStrategy(jwtOptions, async (jwt_payload, done) => {
 		try {
+			// Find a user with the jwt_payload.sub (user._id)
 			const user = await AdminModel.findById(jwt_payload.sub);
 
 			return done(null, user);
