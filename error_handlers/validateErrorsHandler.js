970572473fbb1201f6596fafa2fd1393e3a2f70d
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-03 21:58:08 +0700

    Extract validate error hanlder to a file

---
+++ error_handlers/validateErrorsHandler.js
@@ -0,0 +1,13 @@
+const { check, validationResult } = require("express-validator");
+
+function validateErrorsHandler(req, res, next) {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.mapped() });
+	}
+
+	next();
+}
+
+module.exports = validateErrorsHandler;
