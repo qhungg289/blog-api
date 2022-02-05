70a8efdda6ceaa9c218f0dbaaabb455c68643631
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-05 20:57:39 +0700

    Use all the post related controllers

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,26 @@
 const router = require("express").Router();
+const postsController = require("../controllers/postsController");
 
 // Get lists of all posts
-router.get("/");
+router.get("/", postsController.getAllPosts);
 
 // Create new posts (PROTECTED)
-router.post("/");
+router.post("/", postsController.createNewPost);
 
 // Get one post
-router.get("/:postId");
+router.get("/:postId", postsController.getOnePost);
 
 // Update one post (PROTECTED)
-router.put("/:postId");
+router.put("/:postId", postsController.updateOnePost);
 
 // Delete one post (PROTECTED)
-router.delete("/:postId");
+router.delete("/:postId", postsController.deleteOnePost);
 
 // Get likes count on one post
-router.get("/:postId/likes");
+router.get("/:postId/likes", postsController.getLikesOfPost);
 
 // Update likes count on one post
-router.put("/:postId/likes");
+router.put("/:postId/likes", postsController.updateLikesOfPost);
 
 // Get all comments of one post
 router.get("/posts/:postId/comments");
