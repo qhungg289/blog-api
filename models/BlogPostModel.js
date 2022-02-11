db2afa58fec9ce51af5f27768740eb37a9972f61
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-11 20:47:28 +0700

    Update timestamp property

--- models/BlogPostModel.js
+++ models/BlogPostModel.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose");
 const { DateTime } = require("luxon");
 
-const blogPostSchema = new mongoose.Schema({
-	title: { type: String, required: true },
-	content: { type: String, required: true },
-	likesCount: { type: Number, default: 0, min: 0 },
-	comments: [{ type: mongoose.Types.ObjectId, ref: "Comment" }],
-	publishStatus: { type: Boolean, required: true },
-	createdAt: { type: Date, default: () => DateTime.now() },
-});
+const blogPostSchema = new mongoose.Schema(
+	{
+		title: { type: String, required: true },
+		content: { type: String, required: true },
+		likesCount: { type: Number, default: 0, min: 0 },
+		comments: [{ type: mongoose.Types.ObjectId, ref: "Comment" }],
+		publishStatus: { type: Boolean, required: true },
+	},
+	{ timestamps: { currentTime: () => DateTime.now() } }
+);
 
 blogPostSchema.method("getTimeStampString", function () {
-	return this.createdAt.toLocaleString(DateTime.DATETIME_MED);
-});
-
-blogPostSchema.virtual("url").get(function () {
-	return `/posts/${this._id}`;
+	return this.timestamps.toLocaleString(DateTime.DATETIME_MED);
 });
 
 module.exports = mongoose.model("BlogPost", blogPostSchema);
--- models/CommentModel.js
+++ models/CommentModel.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 const { DateTime } = require("luxon");
 
-const commentSchema = new mongoose.Schema({
-	author: { type: String, default: "Anonymous" },
-	belongToPost: { type: mongoose.Types.ObjectId, ref: "BlogPost" },
-	content: { type: String, required: true },
-	likesCount: { type: Number, default: 0, min: 0 },
-	createdAt: { type: Date, default: () => DateTime.now() },
-});
+const commentSchema = new mongoose.Schema(
+	{
+		author: { type: String, default: "Anonymous" },
+		belongToPost: { type: mongoose.Types.ObjectId, ref: "BlogPost" },
+		content: { type: String, required: true },
+		likesCount: { type: Number, default: 0, min: 0 },
+	},
+	{ timestamps: { currentTime: () => DateTime.now() } }
+);
 
 commentSchema.method("getTimeStampString", function () {
-	return this.createdAt.toLocaleString(DateTime.DATETIME_MED);
+	return this.timestamps.toLocaleString(DateTime.DATETIME_MED);
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
