6cf617b2b30f1c726c866a714680dba78ecbb21e
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-15 10:37:35 +0700

    Add custom validator for sign up logic

--- controllers/adminsController.js
+++ controllers/adminsController.js
@@ -4,40 +4,57 @@ const { check } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
-const validateErrorsHandler = require("../error_handlers/validateErrorsHandler");
+const validateErrorsHandler = require("../middlewares/validateErrorsHandler");
 
 exports.signUp = [
 	// Validate the incoming json
-	check("fullName", "This field is required")
+	check("fullName", "Full name is required")
 		.trim()
 		.escape()
 		.isLength({ min: 1 }),
-	check("username", "Username need to be atleast 4 characters long")
+	check("signUpKey").custom((value) => {
+		if (value != process.env.SIGNUP_SECRET || !value) {
+			throw new Error("Your sign up key is missing or incorrect");
+		}
+
+		return true;
+	}),
+	check("username")
 		.trim()
 		.escape()
-		.isLength({ min: 4 }),
+		.custom(async (value) => {
+			if (value.length < 4) {
+				throw new Error("User name to be atleast 4 characters long");
+			}
+
+			const user = await AdminModel.findOne({ username: value });
+			if (user) {
+				throw new Error("Username already exist");
+			}
+
+			return true;
+		}),
 	check("password", "Password need to be atleast 4 characters long")
 		.trim()
 		.escape()
 		.isLength({ min: 4 }),
+	check("passwordConfirm")
+		.trim()
+		.escape()
+		.custom((value, { req }) => {
+			if (value != req.body.password) {
+				throw new Error(
+					"Password confirmation need to be the same as password"
+				);
+			}
+
+			return true;
+		}),
 	validateErrorsHandler,
 	async (req, res, next) => {
 		try {
 			// Extract all the properties from req.body
-			const { fullName, username, password, signUpKey } = req.body;
-
-			// Check if sign up key is correct to proceed
-			if (signUpKey != process.env.SIGNUP_SECRET || !signUpKey) {
-				return res
-					.status(400)
-					.json({ errors: "Your sign up key is missing or incorrect" });
-			}
-
-			// Check if username already exist or not
-			const u = await AdminModel.findOne({ username: username });
-			if (u) {
-				return res.status(400).json({ errors: "Username already exist" });
-			}
+			const { fullName, username, password } = req.body;
 
 			// Password hashing
 			const hashedPassword = await bcrypt.hash(password, 10);
@@ -72,7 +89,7 @@ exports.logIn = [
 	validateErrorsHandler,
 	// Authenticate with local strategy
 	passport.authenticate("local", { session: false }),
-	(req, res, next) => {
+	(req, res) => {
 		// Extract user property from req object
 		const { user } = req;
 
