7e83523cbf656b86c2ef18604ea1a165f5cec951
Hung Duong Quang <qhungg289@protonmail.com>
2022-02-17 21:37:17 +0700

    Change back to using mapped object in error handler

--- middlewares/validateErrorsHandler.js
+++ middlewares/validateErrorsHandler.js
@@ -4,7 +4,7 @@ function validateErrorsHandler(req, res, next) {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
-		return res.status(400).json({ errors: errors.array() });
+		return res.status(400).json({ errors: errors.mapped() });
 	}
 
 	next();
